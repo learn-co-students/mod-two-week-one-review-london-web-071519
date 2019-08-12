543d14910ad984606d462901815e0530cee88d65
github username <github email address>
2019-08-12 12:19:48 +0100

    work in progress

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/', to: 'seas#welcome', as: 'welcome'
+  get '/seas', to: 'seas#index', as: 'index'
+  post '/seas', to: 'seas#create', as: 'seas'
+  get '/seas/new', to: 'seas#new', as: 'new'
+  get '/seas/:id/edit', to: 'seas#edit', as: 'edit'
+  get '/seas/:id', to: 'seas#show', as: 'show'
+  patch '/seas/:id', to: 'seas#update', as: 'update'
+  delete '/seas/:id', to: 'seas#destroy', as: 'destroy'
+
 end

7d356b1d6981f79b0621bcb50b0f6e01f8632b0d
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-08-13 17:53:32 +0100

    Seven Seas Complete

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,38 @@
 class SeasController < ApplicationController
   #define your controller actions here
+  before_action :sea_path, only: [:show, :edit, :update, :destroy]
+  
+  def welcome
+  end
+  
+  def index
+    @seas = Sea.all
+  end
 
+  def show
+  end
+
+  def new
+    @sea = Sea.new
+  end
+
+  def create
+    @sea = Sea.create(sea_params)
+    redirect_to action: 'show', id:@sea.id
+  end
+
+  def edit
+  end
+
+  def update
+    @sea.update(sea_params)
+    redirect_to action: 'show', id:@sea.id
+  end
+
+  def destroy
+    @sea.destroy
+    redirect_to action: 'index'
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
@@ -10,4 +42,7 @@ class SeasController < ApplicationController
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
 
+  def sea_path
+    @sea = Sea.find(params[:id])
+  end
 end
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,56 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #welcome
+  get '/', to: 'seas#welcome'
+  
+  #index
+  get '/seas', to: 'seas#index'
 
+  #new
+  get '/seas/new' , to: 'seas#new', as: 'new_sea'
+
+  #show
+  get '/seas/:id', to: 'seas#show', as: 'sea'
+
+  #create
+  post '/seas', to: 'seas#create'
+
+  #edit
+  get '/seas/:id/edit', to: 'seas#edit', as: 'edit_sea'
+
+  #update
+  patch '/seas/:id', to: 'seas#update'
+
+  #delete
+  delete 'seas/:id', to: 'seas#destroy'
+
+  # resources :seas, only: [:index, :show, :new, :edit, :update, :create, :destroy]
 end
+
+
+# <%= f.label :name %>
+# <%= f.text_field :name %>
+
+# <%= f.label :temperature %>
+# <%= f.number_field :temperature %>
+
+# <%= f.label :bio %>
+# <%= f.text_area :bio %>
+
+# <%= f.label :image_url %>
+# <%= f.text_field :image_url %>
+
+# <%= f.label :mood %>
+# <%= f.text_field :mood %>
+
+# <%= f.label :favorite_color %>
+# <%= f.text_field :favorite_color %>
+
+# <%= f.label :scariest_creature %>
+# <%= f.text_field :scariest_creature %>
+
+# <%= f.label :has_mermaids %>
+# <%= f.check_box :has_mermaids %>
+
+# <%=  f.submit %>
